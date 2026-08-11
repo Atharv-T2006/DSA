e4cd1361d2b952e475156e89dc22d712c9f981c0
ATHARV_999 <atharvtimane1@gmail.com>
2026-08-11 13:49:41 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (48.31%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+
+        int n=nums.size();
+        int prefix_sum=nums[0];
+        
+       for(int i = 1; i < n; i++) { 
+            if(nums[i]==nums[i-1]+1)
+            {
+                prefix_sum+=nums[i];
+            } 
+            else break;
+              }
+
+        unordered_set<int> s(nums.begin(),nums.end());
+
+        while(s.count(prefix_sum))
+        {
+            prefix_sum++;
+        }
+        
+        return prefix_sum;
+    }
+};
